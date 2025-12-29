03d040561e5fbea8b939f5f0b40b30548f32cfc7
Antigo89 <antigo1989@gmail.com>
2025-12-29 08:27:26 +0300

    added testing led line

---
+++ ledLine.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(12,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(26,GPIO.OUT)
+pwmoutR = GPIO.PWM(13,100)
+pwmoutB = GPIO.PWM(12,100)
+pwmoutG = GPIO.PWM(26,100)
+pwmoutR.start(0)
+pwmoutG.start(0)
+pwmoutB.start(0)
+
+try:
+    while True:
+        for duteCycle in range(0,100,1):
+            pwmoutB.ChangeDutyCycle(duteCycle)
+            pwmoutG.ChangeDutyCycle(100-duteCycle)
+            time.sleep(0.1)
+        for duteCycle in range(100,0,-1):
+            pwmoutB.ChangeDutyCycle(duteCycle)
+            pwmoutG.ChangeDutyCycle(100-duteCycle)
+            time.sleep(0.1)
+except KeyboardInterrupt:
+    pwmoutR.stop()
+    pwmoutG.stop()
+    pwmoutB.stop()
+    GPIO.cleanup()
+    print('exeting')
